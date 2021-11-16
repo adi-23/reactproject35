961e10cc4599f54d2936bd2b1d8a0421b5d0ca08
adi-23 <vallinarasimhaswamy.k19@iiits.in>
2021-11-16 23:04:56 +0530

    created routing for nav components

--- react_project35/src/Nav.js
+++ react_project35/src/Nav.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Home from './Home'
+import Aboutus from './Aboutus';
+import Cart from './Cart';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function Nav() {
     return (
@@ -8,14 +10,17 @@ function Nav() {
             <Router>
                 <div>
                     <Link to="/">Home</Link>
+                    <Link to="/aboutus">Aboutus</Link>
+                    <Link to="/cart">Cart</Link>
+                    
                 </div>
             
             
             
                 <Routes>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
+                 <Route exact path="/" element={<Home />} />
+                 <Route exact path="/aboutus" element={<Aboutus />} />
+                 <Route exact path="/cart" element={<Cart />} />
                 </Routes>
             </Router>
           
